864aab70bd34505795a0e52aaec479b0cc6c504b
Piyush Keshari <piyush.keshari@springworks.in>
2023-07-27 11:15:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const plusButtons = document.getElementsByClassName('plus');
+Array.from(plusButtons).forEach(plusButton => {
+    plusButton.addEventListener('click', function () {
+        const plusButton = this;
+        const question = plusButton.parentNode;
+        const answer = question.nextElementSibling;
+
+        const isInActive = !question.classList.contains('active');
+
+        question.style.fontSize = isInActive ? '1.1rem' : '1.5rem';
+        answer.style.display = isInActive ? 'block' : 'none   ';
+        plusButton.innerText = isInActive ? '-' : '+';
+
+        question.classList.toggle('active');
+    })
+});
